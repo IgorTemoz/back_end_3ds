c8eb24ad03d1ba922070361be017e3e8e1abed11
IgorTemoz <igor.temoz@escola.pr.gov.br>
2026-02-23 12:35:57 +0000

    Criar variaveis para Pessoa e Produto

---
+++ variaveis/variavel-pessoa.js
@@ -0,0 +1,52 @@
+//  , var, let, const (valores reservados da linguagem)
+// 0-9, 0-7, 0-1, 0-9,A,B,C,D,E,F
+
+let nome = 'gallan'             // string
+let sobrenome = 'bhipau'        // string
+const cpf = '67676767676-67'    // string
+const rg = '699669 ssp'         // string
+let idade = 67                  // number
+let solteiro = true             // boolean
+let nota = 6 + 6                // number
+
+console.log(nome)
+// imprima aqui todas as variaveis acima
+
+
+console.log(typeof nome)
+
+console.log(sobrenome)
+// imprima aqui todas as variaveis acima
+
+
+console.log(typeof sobrenome)
+
+console.log(cpf)
+// imprima aqui todas as variaveis acima
+
+
+console.log(typeof cpf)
+
+console.log(rg)
+// imprima aqui todas as variaveis acima
+
+
+console.log(typeof rg)
+
+console.log(idade)
+// imprima aqui todas as variaveis acima
+
+
+console.log(typeof idade)
+
+console.log(solteiro)
+// imprima aqui todas as variaveis acima
+
+
+console.log(typeof solteiro)
+
+console.log(nota)
+// imprima aqui todas as variaveis acima
+
+
+console.log(typeof nota)
---
+++ variaveis/variavel-produto.js
@@ -0,0 +1,21 @@
+const nomeProduto = 'saBOR energetico'
+let preco = '20,00'
+let quantidade = '100'
+let lugar = 'SaoPaulo'
+const marca = 'mansaoMaromba'
+
+console.log(nomeProduto)
+console.log(preco)
+console.log(quantidade)
+console.log(lugar)
+console.log(marca)
+
+console.log("nomeProduto " + nomeProduto)
+console.log("preco " + preco)
+console.log("quantidade " + quantidade)
+console.log("lugar " + lugar)
+console.log("marca " + marca)
+
+
+
+
