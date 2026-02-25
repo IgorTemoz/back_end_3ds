d3bbe1d213bfdb19c942cee159b6b0449a0b667c
IgorTemoz <igor.temoz@escola.pr.gov.br>
2026-02-25 11:35:40 +0000

    sistema de login com senha e login usando if-else

---
+++ condicionais/if_else.js
@@ -0,0 +1,17 @@
+// if, else, else if ,ternário
+// Serve para tomada de decisão (se/senão)
+
+let idadePessoa = 18
+
+if (idadePessoa > 100 ) {
+    console.log("Favor, apresentar acompanhante!")
+    
+} else if (idadePessoa >= 18 && idadePessoa <= 100 ) {
+    console.log("Bem-vindo(a), a festa é sua!")
+    console.log("Festa sugerida > Boate!!!")
+}
+
+else {
+    console.log("Não é festa para menor de idade!")
+    
+}
---
+++ condicionais/if_login.js
@@ -0,0 +1,14 @@
+let login = "vesgo123"
+let senha = "12345"
+
+if (login == "vesgo123") {
+    console.log("login correto")
+} else {
+    console.log("login incorreto")
+}
+
+if (senha == "12325") {
+    console.log("senha correta")
+} else {
+    console.log("Senha Icorreta! ")
+}
