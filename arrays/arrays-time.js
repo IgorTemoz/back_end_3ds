120a8e331ab6eea637858f42753969c7c92585cd
IgorTemoz <igor.temoz@escola.pr.gov.br>
2026-03-04 11:48:44 +0000

    introdução a arrays em js

---
+++ arrays/arrays-time.js
@@ -0,0 +1,11 @@
+const time = ["julius" , "chis"]
+// indices
+time[5] // underfid
+time[6] = "victor" // atribuição dinamicos
+
+for (const jogador of time) {
+    console.log(jogador)
+}
+
+console.log(time)
+console.log(time[1])
