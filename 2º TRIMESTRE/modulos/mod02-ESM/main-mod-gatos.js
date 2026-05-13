e8bf30ae929054b1c4cbcde9eab7594891b19219
IgorTemoz <igor.temoz@escola.pr.gov.br>
2026-05-13 11:43:58 +0000

    finalizaçãp  ESM modulo gatos

---
+++ 2º TRIMESTRE/modulos/mod02-ESM/main-mod-gatos.js
@@ -0,0 +1,10 @@
+
+import {banhoGato, tosaGato, alimentarGato} from './mod-gatos.js'
+
+let racaViraLata = 'Vira-lata'
+
+
+
+console.log(banhoGato('Siamês', 'Completo'))
+console.log(tosaGato('Siamês', 'tosa só as pontas'))
+console.log(alimentarGato('Siamês', 'golden'))
--- 2º TRIMESTRE/modulos/mod02-ESM/mod-gatos.js
+++ 2º TRIMESTRE/modulos/mod02-ESM/mod-gatos.js
@@ -1,13 +1,13 @@
-let racaViraLata = 'Vira-lata'
+
 
  export function banhoGato(racaGato, tipoBanho){
-    
+    return `Gato ${racaGato} fez um tipo de banho: ${tipoBanho}`
 }
 
  export function tosaGato(racaGato, tipotosa){
-    
+    return `Gato ${racaGato} fez um tipo de tosa: ${tipotosa}`
 }
 
 export function alimentarGato(racaGato, tipoRacao){
-    
+    return `Gato ${racaGato} comeu um tipo de racao: ${tipoRacao}`
 }
