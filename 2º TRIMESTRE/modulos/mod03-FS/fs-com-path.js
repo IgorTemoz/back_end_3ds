04b51954558fcfae2efea5e4ecf8b09fff8bb0c3
IgorTemoz <igor.temoz@escola.pr.gov.br>
2026-05-27 11:49:23 +0000

    finalização do modulo FS com path (joins e resolve)

--- 2º TRIMESTRE/modulos/mod03-FS/fs-com-path.js
+++ 2º TRIMESTRE/modulos/mod03-FS/fs-com-path.js
@@ -1,9 +1,13 @@
     // importar PATH e FS
-
-
+    //path possui (join para juntar caminhos e Resolve
+    //para deduzir subdiretórios e S.O.)
+    const fs = require('fs')
+    const path = require('path')
 
     // Salvar o caminho em um variável
+    //const caminho = path.join(__dirname, 'README.md')//Markdown, text, doc
 
+    const caminho = path.resolve(__dirname, ' README.md')// Abrir, ler, deletar, editar
+    const arquivo = fs.readFileSync(caminho, 'utf-8')
+     console.log(arquivo)
 
-
-    // Abrir, ler, deletar, editar
