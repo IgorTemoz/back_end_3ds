1ee2e56d4a3a7abb59dc08f862500599d889b51f
IgorTemoz <igor.temoz@escola.pr.gov.br>
2026-06-02 11:29:40 +0000

    exercicio trabalho filme

---
+++ 2º TRIMESTRE/modulos/mod02-exercicio/main-esm-videos.js
@@ -0,0 +1,6 @@
+import * as locadora from './mod-esm-videos.js'
+
+console.log(locadora.nomeFilme('O senhor dos aneis'))
+console.log(locadora.aluguelfilme('O senhor dos aneis', '17/03/26'))
+console.log(locadora.precofilme('O senhor dos aneis', 'R$9,90'))
+console.log(locadora.devolverfilme('O senhor dos aneis', '30/06/26'))
---
+++ 2º TRIMESTRE/modulos/mod02-exercicio/mod-esm-videos.js
@@ -0,0 +1,18 @@
+ export function nomeFilme(nomeFilme,tipoNome){
+    return ` O nome do filme é: ${nomeFilme} `
+}
+
+ export function aluguelfilme(nomeFilme, tipoAluguel){
+    return `O dia pego do ${nomeFilme} foi: ${tipoAluguel} `
+}
+
+export function precofilme(nomeFilme, tipoPreco){
+    return `O filme ${nomeFilme} custa: ${tipoPreco}`
+    
+}
+
+export function devolverfilme(nomeFilme, tipodevolver){
+    return `O filme ${nomeFilme} sera entregue no dia 
+    ${tipodevolver}`
+    
+}
