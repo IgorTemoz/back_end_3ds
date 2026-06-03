c89440c51de05c0d96dbfd72468306a49997775d
IgorTemoz <igor.temoz@escola.pr.gov.br>
2026-06-03 12:33:14 +0000

    arrumando erros fs path

--- 2º TRIMESTRE/modulos/mod03-FS/fs-com-path.js
+++ 2º TRIMESTRE/modulos/mod03-FS/fs-com-path.js
@@ -1,13 +1,23 @@
-    // importar PATH e FS
-    //path possui (join para juntar caminhos e Resolve
-    //para deduzir subdiretórios e S.O.)
-    const fs = require('fs')
-    const path = require('path')
+// importar PATH e FS
+//path possui (join para juntar caminhos e Resolve
+//para deduzir subdiretórios e S.O.)
+const fs = require('fs')
+const path = require('path')
 
-    // Salvar o caminho em um variável
-    //const caminho = path.join(__dirname, 'README.md')//Markdown, text, doc
-
-    const caminho = path.resolve(__dirname, ' README.md')// Abrir, ler, deletar, editar
+// Salvar o caminho em um variável
+//const caminho = path.join(__dirname, 'README.md')//Markdown, text, doc
+try {
+    const caminho = path.resolve(__dirname, 'README.md')// Abrir, ler, deletar, editar
     const arquivo = fs.readFileSync(caminho, 'utf-8')
-     console.log(arquivo)
+
+    console.log(arquivo)
+} catch (erro) {
+    //console.log(erro)
+    console.log('_______________________')
+    console.warn("Atenção!" +erro.path)
+    throw new Error('Ocorreu um erro inesperado!');
+
+    console.log('__________________________')
+}
+
 
